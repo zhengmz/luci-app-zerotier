829143f51f9ed1c964db4de76a8bce8cc8ccdda4
zhengmz-aliyun <zhengmz@flowsea.com>
2023-06-25 12:43:52 +0800

    fix rmempty bug
    将 rmempty 设为 false，避免选项未展开被删除

--- luasrc/model/cbi/zerotier/settings.lua
+++ luasrc/model/cbi/zerotier/settings.lua
@@ -32,33 +32,33 @@ end
 e = t:option(Value, "port", translate("Port"))
 e.description = translate("Port of zerotier service, default 9993")
 e.placeholder = 9993
-e.rmempty = true
+e.rmempty = false
 e.datatype = "and(port,min(1025))"
 e:depends("more","1")
 
 e = t:option(TextValue, "secret", translate("Secret"))
 e.description = translate("Secret of zerotier client")
-e.rmempty = true
+e.rmempty = false
 e.size = 80
 e:depends("more","1")
 
 e = t:option(Value, "config_path", translate("Configuration folder"))
 e.description = translate("Persistent configuration folder (for ZT controller mode)")
 e.placeholder = "/etc/zerotier"
-e.rmempty = true
+e.rmempty = false
 --allow create
 --e.datatype = "directory"
 e:depends("more","1")
 
 e = t:option(Flag, "copy_config_path", translate("Copy configuration folder"))
 e.description = translate("Copy configuration folder to RAM to prevent writing to flash (for ZT controller mode)")
-e.rmempty = true
+e.rmempty = false
 e:depends("more","1")
 
 e = t:option(Value, "local_conf", translate("Local configuration"))
 e.description = translate("Path to the local.conf")
 e.placeholder = "/etc/zerotier.conf"
-e.rmempty = true
+e.rmempty = false
 e.datatype = "file"
 e:depends("more","1")
 
